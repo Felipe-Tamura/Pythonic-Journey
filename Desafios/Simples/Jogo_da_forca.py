38897fa8f412f61c6f9bd004930a66c2d398c561
Felipe Pereira Tamura <felipe.tamura@nsnet.local>
2024-01-17 17:40:22 -0300

    Modificando código da forca

--- Desafios/Simples/Jogo_da_forca.py
+++ Desafios/Simples/Jogo_da_forca.py
@@ -1,8 +1,8 @@
 lista_de_palavras = ['Sao Paulo']
 
+texto = ['u']
+texto.append('a')
+
 for i in lista_de_palavras[0]:
-    texto = 'a'
-    print(('_' if i != texto else texto) * len(i.split()), end=' ' if i != ' ' else '* ')
+    print(('_' if i != texto[1] else texto[1]) * len(i.split()), end=' ' if i != ' ' else '* ')
     
-    # if texto == i:
-    #     print('_' * len(i) + texto, end=' ')
