9712821d6700647e0383bc7e10403eba54aa07f6
Felipe Pereira Tamura <felipe.tamura@nsnet.local>
2024-01-19 17:31:47 -0300

    Colocando banco de dados e escolhendo aleatoriamente uma palavra

--- Desafios/Simples/Jogo_da_forca.py
+++ Desafios/Simples/Jogo_da_forca.py
@@ -6,8 +6,20 @@ def limparTela():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def palavra_aleatoria():
-    palavras_tabela = pd.Series(['Sao paulo'])
-    return rnd.choice(palavras_tabela)
+    tabela = pd.DataFrame(
+        {
+            'Cidade': ['Sao paulo', 'Campinas', 'Osasco', 'Pinheiro', 'Cotia'],
+            'Comida':['Lasanha', 'Feijoada', 'Macarrão', 'Bife', 'Queijo'],
+            'Objeto': ['Lapis', 'Garfo', 'Colher', 'Papel', 'Mesa'],
+            'Filme': ['Fuga das galinhas', 'Jhon Wick', 'Harry Potter', 'A casa do Mickey Mouse', 'Matrix'],
+            'Anime': ['Naruto', 'Hunter x Hunter', 'Bleach', 'Death Note', 'Noragami'],
+        }
+    )
+    random_column = rnd.randint(0, 4)
+    random_row = rnd.randint(0, 4)
+    
+    print(tabela.columns[random_column])
+    return tabela.iat[random_row, random_column]
 
 limparTela()
 
@@ -36,8 +48,9 @@ def jogo():
     for i in range(len(forca)):
         if palavra_escolhida[i] == ' ':
             forca[i] = ' '
-        print(forca[i], end=' * ' if forca[i] == ' ' else ' ')
+        print(forca[i], end='* ' if forca[i] == ' ' else ' ')
     
+    #Loop infinito
     while vidas > 0:
         print(' ' * 50, f'Vidas: {vidas}')
         numero_ocorrencia = int(0)
@@ -50,6 +63,7 @@ def jogo():
                 numero_ocorrencia += 1
                 if resultado.join(forca) == palavra_escolhida:
                     print('Você venceu!!! Parabéns!!!!')
+                    print(f'"{resultado.join(forca)}" era a resposta')
                     menu()
                     break
         else:
@@ -58,7 +72,7 @@ def jogo():
         for i in range(len(forca)):
             if palavra_escolhida[i] == ' ':
                 forca[i] = ' '
-            print(forca[i], end=' * ' if forca[i] == ' ' else ' ')
+            print(forca[i], end='* ' if forca[i] == ' ' else ' ')
         
         if vidas == 0:
             print(f'\nA palavra escolhida era: {str(palavra_escolhida)}')
