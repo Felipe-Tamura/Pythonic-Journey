36ef90768546b5ab6cf32d947a7165e5345aeb64
Felipe Pereira Tamura <felipe.tamura@nsnet.local>
2024-01-24 09:21:50 -0300

    Finalizando desafio de manipulação de arquivos

--- Desafios/Simples/Manipulando_arquivos.py
+++ Desafios/Simples/Manipulando_arquivos.py
@@ -11,13 +11,27 @@ lista_de_palavras = []
 for i in arquivo:
     palavras = i.split()
     for j in range(len(palavras)):
-        for k in range(len(palavras)):
-            if palavras[j] == palavras[k]:
-                quantidade += 1
-        print(f'A palavra \'{palavras[j]}\' tem {quantidade}', 'repetições' if quantidade > 1 else 'repetição')
-        lista_de_quantidades.append(quantidade)
-        lista_de_palavras.append(palavras[j])
-        quantidade = 0
+        if palavras[j] not in lista_de_palavras:
+            for k in range(len(palavras)):
+                if palavras[j] == palavras[k]:
+                    quantidade += 1
+            lista_de_quantidades.append(quantidade)
+            lista_de_palavras.append(palavras[j])
+            quantidade = 0
 
-print(lista_de_palavras)
-print(lista_de_quantidades)
+def organizando_quantidades(valores):
+    
+    for i in range(len(valores) -1):
+        trocou = False
+        for j in range(len(valores) -i -1):
+            if valores[j] > valores[j + 1]:
+                valores[j], valores[j + 1] = valores[j + 1], valores[j]
+                trocou = True
+        if not trocou:
+            break
+        
+    return valores[-1]
+
+numero_indice = organizando_quantidades(lista_de_quantidades.copy())
+resultado_final = lista_de_palavras[lista_de_quantidades.index(numero_indice)]
+print(f'A palavra/letra mais comum é:', resultado_final)
