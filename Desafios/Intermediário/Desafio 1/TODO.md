dbbbfe524fc8c6ff681633a177dab71e6a90e002
Felipe Pereira Tamura <felipe.tamura@nsnet.local>
2024-02-20 12:07:12 -0300

    Arrumando a criação, a exclusão e adicionando a edição

--- Desafios/Intermediário/Desafio 1/TODO.md
+++ Desafios/Intermediário/Desafio 1/TODO.md
@@ -1,20 +1,15 @@
-1. **Desafio de Gerenciamento de Contatos:**
+## 1. **Desafio de Gerenciamento de Contatos:**
     - Crie um programa que permita ao usuário adicionar, visualizar, 
     editar e excluir contatos. Use estruturas de dados adequadas para 
     armazenar as informações.
 
-##### Visualização do Banco de Dados
-- Visualizar -> UI
+### Visualização do Banco de Dados
+    - Visualizar -> UI
 
 ### Banco de Dados
-- Arquivo CSV
+    - Arquivo CSV: contato.csv
 
-##### Interação com Banco de Dados
-- Adicionar
-- Editar
-- Excluir
-
-<p>
-Vamos adicionar um banco de dados que puxa dados de um arquivo csv;
-Vamos adicionar um UI que mostre o contato ou contatos de interesse.
-</p>
+### Interação com Banco de Dados
+    - Adicionar
+    - Editar
+    - Excluir
--- Desafios/Intermediário/Desafio 1/bd.py
+++ Desafios/Intermediário/Desafio 1/bd.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 import pandas as pd
 import csv
 import re
+import os
 
 
 @dataclass
@@ -12,7 +13,7 @@ class banco_de_dados:
     arquivo = 'Desafios/Intermediário/Desafio 1/contato.csv'
 
     @classmethod
-    def adicionar(cls, nome, numero, email):
+    def adicionar(cls, nome: str, numero: str, email: str):
         """ 
             Adicionando contato no arquivo
             Args:
@@ -22,10 +23,10 @@ class banco_de_dados:
         """
 
         try:
-            valid = cls.__validar_email(email)
-            if not valid:
-                print(f'E-mail \'{email}\' inválido, tente outro e-mail!')
-            else:
+            valid_email = cls.__validar_email(email)
+            valid_tel = cls.__validar_telefone(numero)
+
+            if valid_email & valid_tel:
                 tab_contato = pd.read_csv(cls.arquivo)
                 dado = (
                     {
@@ -34,8 +35,10 @@ class banco_de_dados:
                         'Email': email
                     }
                 )
-                if numero in tab_contato.Numero:
-                    print('Esta conta já existe')
+                for lista in tab_contato.Numero:
+                    if lista == numero:
+                        print(f'Número \'{numero}\' já adicionado')
+                        break
                 else:
                     tab_contato.loc[len(tab_contato)] = dado
                     tab_contato.to_csv(cls.arquivo, index=False)
@@ -45,6 +48,9 @@ class banco_de_dados:
         except Exception as e:
             print(f'Erro inesperado: {e}')
 
+    def editar(self, dado_para_editar):
+        pass
+    
     def excluir(self, dado_para_excluir):
         """ 
             Excluindo um contato da lista de contatos
@@ -53,11 +59,20 @@ class banco_de_dados:
                     deseja excluir
         """
         lista = []
-
+        apagar_indice = 1
+        
         try:
             # Lendo o arquivo csv
-            self.arquivo
-            #Apagando o item do arquivo
+            with open(self.arquivo, 'r', newline='') as arquivo_entrada:
+                leitor = csv.reader(arquivo_entrada)
+                for listagem in leitor:
+                    if listagem[apagar_indice] != dado_para_excluir:
+                        lista.append(listagem)
+            
+            # Sobrescrevendo arquivo com dados da lista
+            with open(self.arquivo, 'w', newline='') as arquivo_saida:
+                escritor = csv.writer(arquivo_saida)
+                escritor.writerows(lista)
 
         except FileNotFoundError:
             print(f'O arquivo \'{self.arquivo}\' não foi encontrado')
@@ -67,19 +82,64 @@ class banco_de_dados:
     # Verificando se o email é válido
     @classmethod
     def __validar_email(cls, email_de_verificacao):
+        """ 
+            Verificando se o padrão do email está válido
+            Args:
+                - email_de_verificacao: Email para validação
+        """
+        
         padrao = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+        # ^: Considerar tudo que está nos colchetes
+        # \w: Digitos alfanumerios
+        # \.: Caso haja ponto
+        # -: Caso haja hifen
+        # +: Concatenação
+        # @: Literal (Literalmente o @)
+        # $: Fim do padrão
+
+        # Procurando um padrão no email_de_verificacao
         verif = re.match(padrao, email_de_verificacao)
         if verif:
             return True
         else:
+            print(f'Email \'{email_de_verificacao}\' inválido, tente outro email!!')
             return False
     
     # Verificando se o número de telefone é válido
-    def __validar_telefone():
-        pass
+    @classmethod
+    def __validar_telefone(cls, telefone_de_verificacao):
+        """ 
+            Verificando se o padrão do telefone é válido
+            Args:
+                - telefone_de_verificacao: Telefone para validação
+        """
+        padrao = r'\d{2} 9\d{8}$'
+        # \d: Digitos
+        # {<digito>}: Quantidade de digitos que pode ter
+        # 9: Literal
+        
+        # Procurando um padrão no telefone_de_verificacao
+        verifi = re.match(padrao, telefone_de_verificacao)
+        if verifi:
+            return True
+        else:
+            print(f'Telefone \'{telefone_de_verificacao}\' inválido, tente outro telefone')
+            return False
+    
+    @property
+    def limpar_tela(self):
+        """ 
+            Para limpar o terminal.
+            
+            Chamamos esta função como se fosse
+            uma propriedade, sem abrir os parênteses
+        """
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 if __name__ == '__main__':
 
     bd = banco_de_dados()
-    contato3 = bd.adicionar('Felipe', 432895, 'felipe@gmail.com')
-    bd.excluir('Felipe')
+    bd.limpar_tela
+    contato3 = bd.adicionar('Felipe', '11 954432895', 'felipe@gmail.com')
+    contato2 = bd.adicionar('Felipe', '11 912345678', 'felipe@gmail.com')
+    bd.excluir('11 912345678')
