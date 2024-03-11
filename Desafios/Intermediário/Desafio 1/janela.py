ec6393e19c89c5ebc79a9ee14cfd1a741234d971
Felipe Pereira Tamura <felipe.tamura@nsnet.local>
2024-03-11 11:06:20 -0300

    Criando uma classe para a nova janela de adicioncar

--- Desafios/Intermediário/Desafio 1/janela.py
+++ Desafios/Intermediário/Desafio 1/janela.py
@@ -1,4 +1,4 @@
-import csv
+import pandas
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
@@ -36,16 +36,19 @@ class Janela(tk.Tk):
         self.cena_esquerda = tk.Frame(self, bg=cores.fundo_cena, padx = 5, pady = 5)
         self.cena_esquerda.grid(row = 0, column = 0, padx = 5)
         
-        # Garantindo que o nome do cabeçalho das colunas sejam a primeira
-        # linha do arquivo csv
-        dados = self.ler_dado()
-        
+        # Criando uma lista de item para adicionar na listbox
+        dados = self.__ler_dado()
+        lista_item = tk.Variable(value=dados)
+
         # Criando colunas
-        self.treeview = ttk.Treeview(self.cena_direita, columns=(dados[0]), height = 12, show="headings")
-        self.treeview.grid(row = 0, column = 1, columnspan = 4, rowspan = 4, pady=5, padx=5)
-        
-        # Chamando a função para adicionar tudo nas colunas
-        self.exibir_dados_lista()
+        self.caixa_lista = tk.Listbox(
+            self.cena_direita,
+            height = 10,
+            width = 75,
+            listvariable = lista_item,
+            selectmode = tk.SINGLE
+        )
+        self.caixa_lista.grid(pady = 5, padx = 5)
         
         # Criando um botão para adicionar um novo contato
         bt_add = tk.Button(
@@ -73,7 +76,7 @@ class Janela(tk.Tk):
             activebackground = cores.fundo_botao_click, # Referente a quando clicamos no botão
             activeforeground = cores.fonte_botao_click # Referente a quando clicamos no botão
         )
-        bt_del.grid(row = 1, column = 0, pady = 5)
+        bt_del.grid(row = 2, column = 0, pady = 5)
 
         # Criando um botão para editar o contato selecionado
         bt_edit = tk.Button(
@@ -87,50 +90,17 @@ class Janela(tk.Tk):
             activebackground = cores.fundo_botao_click, # Referente a quando clicamos no botão
             activeforeground = cores.fonte_botao_click # Referente a quando clicamos no botão
         )
-        bt_edit.grid(row = 2, column = 0, sticky=tk.N, pady = 5)
+        bt_edit.grid(row = 1, column = 0, sticky=tk.N, pady = 5)
 
-    def exibir_dados_lista(self):
-        """ 
-            Adicionando itens a caixa_lista e retornando a exibição
-        """
-        # Instanciando os dados
-        dados = self.ler_dado()
-
-        # Limpando lista antiga
-        for row in self.treeview.get_children():
-            self.treeview.delete(row)
-        
-        # Criando uma função para adicionar nome a coluna
-        adicionar_nome = lambda coluna: self.treeview.heading(coluna, text=coluna)
-        tamanho_coluna = lambda width_coluna: self.treeview.column(width_coluna, width = 150)
-
-        # Iterando/mapeando lista de colunas e adicionando nome da coluna
-        list(map(adicionar_nome, dados[0]))
-        list(map(tamanho_coluna, dados[0]))
-
-        # Criando uma função para adicionar novos itens a lista
-        adicionar = lambda item: self.treeview.insert('', tk.END, values=item)
-         
-        # Iterando/Mapeando lista de dados e adicionando itens
-        list(map(adicionar, dados[1]))
-
-    def ler_dado(self):
+    def __ler_dado(self):
         """ 
             Lendo o arquivo csv e adicionando os dados em uma lista
         """
         # Lendo arquivo
-        with open(self.arquivo, 'r') as arquivo_entrada:
-            leitor = csv.reader(arquivo_entrada)
-            
-            # Lendo nome das colunas
-            nome_coluna = next(leitor)
+        df = pandas.read_csv(self.arquivo)
+        nomes = list(x for x in df['Nome'])
+        return nomes
 
-            # Realizando iteração e adicionando na lista
-            dados = [row for row in leitor]
-            
-        # Retornando tudo em uma tupla
-        return nome_coluna, dados
-    
     def botao_adicionar(self):
         """ 
             Este comando faz com que adicione um novo contato no arquivo
@@ -140,22 +110,52 @@ class Janela(tk.Tk):
             bd onde adiciona definitivo o contato no arquivo.
         """
         # Criando uma nova janela de formuário
-        forms = tk.Toplevel(self, padx = 10, pady = 10, bg = cores.fundo_janela)
+        forms = tk.Toplevel(
+            self,
+            padx = 10,
+            pady = 10,
+            bg = cores.fundo_janela
+        )
         # Focando na janela do formulário
         forms.transient(self)
         # Colocando um titulo
         forms.title("Adicionar Novo Contato")
 
         # Criando 2 frames para o formulário
-        frame_forms_cima = tk.Frame(forms, bg = cores.fundo_cena, pady = 5, padx = 5)
-        frame_forms_baixo = tk.Frame(forms, bg = cores.fundo_cena, pady = 5, padx = 5)
+        frame_forms_cima = tk.Frame(
+            forms,
+            bg = cores.fundo_cena,
+            pady = 5,
+            padx = 5
+        )
+        frame_forms_baixo = tk.Frame(
+            forms,
+            bg = cores.fundo_cena,
+            pady = 5,
+            padx = 5
+        )
         frame_forms_cima.grid(row = 0, column = 0, pady = 5, padx = 5)
         frame_forms_baixo.grid(row = 1, column = 0, pady = 5, padx = 5)
 
         # Criando 3 rotulos com informação sobre o que colocar na entrada
-        rl_nome = tk.Label(frame_forms_cima, text="Nome:", bg = cores.fundo_cena, foreground = cores.fonte_rotulo)
-        rl_numero = tk.Label(frame_forms_cima, text="Número:", bg = cores.fundo_cena, foreground = cores.fonte_rotulo)
-        rl_email = tk.Label(frame_forms_cima, text="Email:", bg = cores.fundo_cena, foreground = cores.fonte_rotulo)
+        rl_nome = tk.Label(
+            frame_forms_cima,
+            text="Nome:",
+            bg = cores.fundo_cena,
+            foreground = cores.fonte_rotulo
+        )
+        rl_numero = tk.Label(
+            frame_forms_cima,
+            text="Número:",
+            bg = cores.fundo_cena,
+            foreground = cores.fonte_rotulo
+        )
+        rl_email = tk.Label(
+            frame_forms_cima,
+            text="Email:",
+            bg = cores.fundo_cena,
+            foreground = cores.fonte_rotulo
+        )
         # Adicionando na grade
         rl_nome.grid(row = 0, column = 0, sticky = tk.E)
         rl_numero.grid(row = 1, column = 0, sticky = tk.E)
@@ -199,12 +199,6 @@ class Janela(tk.Tk):
                     numero = ent_numero.get(),
                     email = ent_email.get()
                 )
-
-                # Atualizando a janela
-                self.exibir_dados_lista()
-
-                # Fechando o formulário
-                forms.destroy()
                 
             else:
                 messagebox.showinfo(title = "AVISO!", message = "Favor preencher todos os campos")
@@ -242,38 +236,14 @@ class Janela(tk.Tk):
         """ 
             Por enquanto usando para depuração
         """
-        selecao = self.treeview.selection()[0]
-        filho = self.treeview.get_children()[0]
-        print(
-            selecao,
-            filho
-        )
+        dados = self.__ler_dado()
+        print(dados)
     
     def botao_excluir(self):
         """ 
             Esta função deleta o contato do arquivo ao selecionar o
             contato da lista
         """
-        try:
-            # Instanciando a classe de banco de dados
-            banco_dados = bd.banco_de_dados()
-            
-            # Pegando o ID do item selecionado na lista
-            selecao = self.treeview.selection()[0]
-            print(selecao)
-            # Excluindo o item do arquivo usando a função do banco de dados
-            banco_dados.excluir(self.ler_dado()[1][int(selecao[3]) - 1][1])
-            
-            # 1. Aqui estamos selecionando o segundo item da tupla: [1]
-            # 2. Selecionando o item da lista dentro da tupla selecionada
-            # logo de primeira: [int(selecao[3]) - 1] (Colocamos o -1
-            # pois esta parte estamos retornando um número fora da lista)
-            # 3. Por fim selecionamos a coluna do número para exclui-lo: [1]
-
-        except Exception as x:
-
-            print(selecao)
-            print(f"Erro inesperado: {x}")
 
     @property
     def limpar_console(self):
